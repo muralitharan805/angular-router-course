7c1c68cbd9f0f6174a6d200febcf5ffa37885892
muralitharan0506 <muralitharan@aagnia.com>
2021-11-05 13:52:48 +0530

    page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"/courses",
+    pathMatch:"full"
+
+  },
   {
     path:"courses",
     loadChildren:()=>import('./courses/courses.module').then(m=>m.CoursesModule)
@@ -17,7 +24,12 @@ const routes: Routes = [
     path:"about",
     component:AboutComponent
   },
-  { path: 'demo_module2', loadChildren: () => import('./demo-module2/demo-module2.module').then(m => m.DemoModule2Module) }
+  { path: 'demo_module2', loadChildren: () => import('./demo-module2/demo-module2.module').then(m => m.DemoModule2Module) },
+  {
+    path:"**",
+    component:PageNotFoundComponent
+  }
+
 ];
 
 @NgModule({
