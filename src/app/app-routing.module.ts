264fa5dc11e13344416664db9928fdd8044c507e
muralitharan0506 <muralitharan@aagnia.com>
2021-11-06 19:42:35 +0530

    secondary outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CanLoadGuard } from './services/canload';
@@ -25,6 +26,11 @@ const routes: Routes = [
   {
     path:"about",
     component:AboutComponent
+  },
+  {
+    path:"helpdesk-chat",
+    component:ChatComponent,
+    outlet:'chat'
   },
   { path: 'demo_module2', loadChildren: () => import('./demo-module2/demo-module2.module').then(m => m.DemoModule2Module) },
   {
